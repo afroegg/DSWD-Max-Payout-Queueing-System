72487d42504ec7050c34a207b829b3e74509c0be
afroegg <128572015+afroegg@users.noreply.github.com>
2026-04-12 22:27:01 +0800

    Update db.php

--- dswd_max_payout/config/db.php
+++ dswd_max_payout/config/db.php
@@ -1,19 +1,15 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "dswd_max_payout";
 
 $conn = new mysqli(
-    "HOST_FROM_PROVIDER",
-    "USERNAME",
-    "PASSWORD",
-    "DATABASE"
+    "metro.proxy.rlwy.net",
+    "root",
+    "UMiFmmqBGdmWSduxuNrmXuEYMifHcyqu",
+    "railway",
+    19599
 );
 
 if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-date_default_timezone_set('Asia/Manila');
 ?>
