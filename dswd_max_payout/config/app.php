0c3f3e8206fade596a4de61ae7251bd04d7508be
afroegg <128572015+afroegg@users.noreply.github.com>
2026-04-12 22:09:48 +0800

    Update app.php

--- dswd_max_payout/config/app.php
+++ dswd_max_payout/config/app.php
@@ -1,3 +1,3 @@
 <?php
-define('BASE_URL', 'http://localhost/dswd_max_payout');
+define('BASE_URL', 'https://your-render-url.onrender.com');
 define('SEMAPHORE_API_KEY', 'PUT_YOUR_SEMAPHORE_API_KEY_HERE');
