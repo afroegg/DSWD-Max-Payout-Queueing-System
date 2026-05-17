03624af89cc5c67379f8a46b362219d17eadfe05
afroegg <128572015+afroegg@users.noreply.github.com>
2026-05-17 19:22:54 +0800

    Convert placeholder step2 page to assessment redirect

--- dswd_max_payout/staff/step2.php
+++ dswd_max_payout/staff/step2.php
@@ -1,19 +1,5 @@
 <?php
 include('../auth/check.php');
-include('../config/db.php');
+header('Location: assessment_screen.php');
+exit;
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Step 2</title>
-<link rel="stylesheet" href="../assets/style.css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
-<body>
-<div class="app">
-<main class="main"><section class="content"><h1>Step 2</h1></section></main>
-<?php include('sidebar.php'); ?>
-</div>
-</body>
-</html>
