93281057bed14696208c65911d1a9beb75279eca
afroegg <128572015+afroegg@users.noreply.github.com>
2026-05-27 21:20:52 +0800

    Delete dswd_max_payout directory

--- dswd_max_payout/api/remove_queue.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('../auth/check.php');
-header('Location: ../staff/verifier.php');
-exit;
-?>
--- dswd_max_payout/staff/counter.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('../auth/check.php');
-header('Location: assessment_screen.php');
-exit;
-?>
